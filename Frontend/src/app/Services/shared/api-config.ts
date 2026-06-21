4abaa63a05c564cf762a377b403f830634014993
Filippo Orsi <152084214+forsinu@users.noreply.github.com>
2026-06-21 17:02:10 +0200

    Update api-config.ts -> from https to http for self signed certificate issues

--- Frontend/src/app/Services/shared/api-config.ts
+++ Frontend/src/app/Services/shared/api-config.ts
@@ -1,4 +1,4 @@
-export const API_HOST = 'https://app.docker.localhost';
+export const API_HOST = 'http://app.docker.localhost';
 
 export const AUTH_API_BASE_URL = `${API_HOST}/auth/api/v1`;
 export const FEDERATION_API_BASE_URL = `${API_HOST}/fed/api/v1`;
